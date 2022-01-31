b3463efb1a3ce75adfd15ab44897ea36a6af5ac6
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-01-31 14:16:55 -0500

    Code Snapshot 6.1

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,22 +1,14 @@
-var x;
-var y;
-var z;
-console.log("one");
-
 var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = `https://api.github.com/users/${user}/repos`;
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-        console.log(data);
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
     });
-    });
+  });
 };
-console.log("three");
 
-getUserRepos("H0RSESH0E");
-
-console.log("four");
-
-// getUserRepos();
+getUserRepos("lernantino");
